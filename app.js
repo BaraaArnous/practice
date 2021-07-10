a645b4138b7966f1d0ed036b56634bc8594a24c3
BaraaArnous <baraa.arnous@gmail.com>
2021-07-10 21:29:47 +0300

    new

---
+++ app.js
@@ -0,0 +1,76 @@
+'use strict'
+
+var baraa = 'hello'
+
+console.log(baraa);
+
+
+
+console.log(baraa);
+
+var baraa = 'hello' 
+
+*// is declaerated is the same thing 
+*// use console .log brfore the varible or after
+
+or 
+
+var baraa;
+console.log(baraa);
+hero = 'hello';
+
+let baraa 'hello';
+let baraa;
+
+var x =10;
+var x =3;
+
+let user = 'baraa';
+
+if(true);{
+    console.log(user);
+
+}
+if(true){
+    const lastname= 'arnous';
+}
+console.log(lastname);
+
+
+
+console.log(hello baraa);
+
+/* refrence errors not defined value*/
+
+let arr [1,2,3,4,5,6];
+
+arr.length = 99*99;
+
+range error();
+
+/* syntax error some think not understanding in java*/
+
+lrt x = 10;
+
+/* Tybe error  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
